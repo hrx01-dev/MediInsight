e8253ce977fa370d5cb71f376b139ea3b304aabc
Jayam Srivastava <jayamsrivastava99@gmail.com>
2026-02-27 19:11:57 +0530

    fixed a minor issue

--- app/src/main/java/navigation/routes.kt
+++ app/src/main/java/navigation/routes.kt
@@ -12,6 +12,5 @@ object Routes {
     const val Chat = "chat"
     const val MedicineScanner = "medicine_scanner"
     const val VoiceAssistant = "voice_assistant"
-    const val MedicineAnalysis = "medicine_analysis"
-    const val MedicineAnalysisResults = "medicine_analysis_results"
+
 }
